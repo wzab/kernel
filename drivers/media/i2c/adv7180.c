da8a710c4acdfdd00e2ceb27b430b0de994a5887
Wojciech M. Zabolotny <wzab01@gmail.com>
2020-01-18 19:08:48 +0100

    Poprzednia łata zapewniła dobre ustawienie trybu, ale nadal się nie strumieniuje. Próbuję wrócić do pixel rate 13,5 MHz (w końcu w trybie alternate takie powinno być?)

--- drivers/media/i2c/adv7180.c
+++ drivers/media/i2c/adv7180.c
@@ -579,7 +579,7 @@ static int adv7180_init_controls(struct adv7180_state *state)
 	if (state->link_freq)
 		state->link_freq->flags |= V4L2_CTRL_FLAG_READ_ONLY;
 	//pixel_rate = mode->vts_def * mode->hts_def * mode->max_fps;
-	pixel_rate = ADV7180_LINK_FREQ_108MHZ * 2 / 8;
+	pixel_rate = ADV7180_LINK_FREQ_108MHZ * 2 / 16;
 	state->pixel_rate = v4l2_ctrl_new_std(&state->ctrl_hdl, NULL, V4L2_CID_PIXEL_RATE, 0, pixel_rate,
 					      1, pixel_rate);
 	state->sd.ctrl_handler = &state->ctrl_hdl;
@@ -714,7 +714,7 @@ static int adv7180_set_pad_format(struct v4l2_subdev *sd,
 		framefmt = v4l2_subdev_get_try_format(sd, cfg, 0);
 		*framefmt = format->format;
 	}
-	pixel_rate = ADV7180_LINK_FREQ_108MHZ * 2 / 8;
+	pixel_rate = ADV7180_LINK_FREQ_108MHZ * 2 / 16;
 	__v4l2_ctrl_modify_range(state->pixel_rate, pixel_rate,
 				 pixel_rate, 1, pixel_rate);
 	
